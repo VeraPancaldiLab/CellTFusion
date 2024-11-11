929d4e3df088b4ffd3ed7020ddbc37a2fbd414a3
mhurtado13 <marcelo.hurt137@gmail.com>
2024-11-11 15:18:56 +0100

    Start roxygen2 struture

--- R/calculate_accuracy.R
+++ R/calculate_accuracy.R
@@ -1,3 +1,16 @@
+#' Calculates accuracy values from prediction
+#'
+#' @param metrics A Dataframe with metrics obtained using get_sensitivity_specificity() function
+#' @param target A character vector containing the true values from the target variable
+#'
+#' @return A numeric vector with the accuracy values
+#' @export
+#'
+#' @examples
+#'
+#'observed_values = c("yes", "yes", "no", "yes")
+#'accuracy = calculate_accuracy(metrics, observed_values)
+#'
 calculate_accuracy <- function(metrics, target) {
   sensitivity = metrics[,"sensitivity"]
   specificity = metrics[,"specificity"]
