ba15675a330bc3be8945373800c6ba7df20d8741
mhurtado13 <marcelo.hurt137@gmail.com>
2025-11-06 15:19:13 +0100

    Add DeconRNAseq for multideconv

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,8 @@
 .onLoad <- function(libname, pkgname) {
   results_dir <- file.path("Results")
   dir.create(results_dir, showWarnings = FALSE, recursive = TRUE)
+
+  if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  BiocManager::install("DeconRNASeq", force = TRUE)
 }
