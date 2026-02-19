7fe392281327032102e4844af421672a49a38dd5
mhurtado13 <marcelo.hurt137@gmail.com>
2026-02-19 10:59:39 +0100

    Correct replicate cell groups

--- R/CellTFusion.R
+++ R/CellTFusion.R
@@ -1874,7 +1874,16 @@ compute.test.set = function(deconv_res, cell_groups, features, deconvolution_tes
 
       deconv_subgroups_values = c()
       for (i in 1:length(base_groups)) {
-        deconv_subgroups_values = cbind(deconv_subgroups_values, matrixStats::rowMedians(as.matrix(deconvolution_test[,base_groups[[i]]]))) #Compute median using base groups
+
+        x = as.matrix(deconvolution_test[, colnames(deconvolution_test) %in% base_groups[[i]], drop = FALSE])
+
+        if(ncol(x) == 0){
+          med = rep(0, nrow(deconvolution_test))
+        } else {
+          med = matrixStats::rowMedians(x)
+        }
+
+        deconv_subgroups_values = cbind(deconv_subgroups_values, med) #Compute median using base groups
       }
       colnames(deconv_subgroups_values) = names(base_groups)
       deconvolution_test = cbind(deconv_subgroups_values, deconvolution_test) # Join cell subgroups and deconv features
@@ -1883,7 +1892,7 @@ compute.test.set = function(deconv_res, cell_groups, features, deconvolution_tes
 
     if(any(has_clusters)){
       ## Paste the corresponding clusters to the deconvolution features
-      colnames(deconvolution_test) <- paste0(colnames(deconvolution_test), map$suffix[match(colnames(deconvolution_test), map$base)])
+      colnames(deconvolution_test) <- paste0(colnames(deconvolution_test), "_", map$suffix[match(colnames(deconvolution_test), map$base)])
     }
 
     deconvolution_test = deconvolution_test[,colnames(deconvolution_test)%in%colnames(deconv_res[[1]])]
